a9d36a557c3c92008bb3f1a04bfcc8304f8aee0b
ahaandesai27 <ahaan.desai2705@gmail.com>
2024-10-27 18:37:29 +0530

    Removed public, views, added more urls for allowedorigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,10 @@
 const allowedOrigins = [
     'http://localhost:3500', 
     'http://localhost:3000', 
-    'http://localhost:4200'
+    'http://localhost:4200',
+    'http://localhost:5000',
+    'http://localhost:8000',
+    'http://localhost:8001'
 ];
 
 module.exports = allowedOrigins;
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-    origin: function (origin, callback) {
+    origin: (origin, callback) => {
         console.log('origin: ', origin);
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
